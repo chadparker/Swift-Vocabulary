7ce61e0088be8d8fece64b78f4876d7191a12f28
Chad Parker <chad@chadparker.com>
2020-01-29 18:58:26 -0800

    Add model file VocabularyWord.swift

---
+++ Swift Vocabulary/Model/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Chad Parker on 1/29/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
